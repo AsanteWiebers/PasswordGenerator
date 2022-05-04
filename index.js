d0a2d22e2936ce93738dc441796994fe979f9a9c
Asante Wiebers <67924684+AsanteWiebers@users.noreply.github.com>
2022-05-04 21:56:41 +0200

    test

---
+++ index.js
@@ -0,0 +1,5 @@
+const passWord1 = document.getElementById("pw-elem-1");
+const passWord2 = document.getElementById("pw-elem-2");
+const passWord3 = document.getElementById("pw-elem-3");
+const passWord4 = document.getElementById("pw-elem-4");
+const chars = []
